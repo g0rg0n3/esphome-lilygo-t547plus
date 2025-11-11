0b4ad125a30ab4b7f7d4db6d7436bc22ec2beaca
G0RG0Ne <154641869+g0rg0n3@users.noreply.github.com>
2025-11-11 09:35:48 +0100

    Update t547.h
    update .h for esphome compat

--- components/t547/t547.h
+++ components/t547/t547.h
@@ -7,62 +7,46 @@
 
 #include "epd_driver.h"
 
-#ifdef USE_ESP32_FRAMEWORK_ARDUINO
-
 namespace esphome {
 namespace t547 {
 
-#if ESPHOME_VERSION_CODE >= VERSION_CODE(2023, 12, 0)
-class T547 : public display::DisplayBuffer {
-#else
 class T547 : public PollingComponent, public display::DisplayBuffer {
-#endif  // VERSION_CODE(2023, 12, 0)
  public:
-  void set_greyscale(bool greyscale) {
-    this->greyscale_ = greyscale;
-  }
+  T547() = default;
 
+  void set_greyscale(bool greyscale) { this->greyscale_ = greyscale; }
+
+  // Component methods
   float get_setup_priority() const override;
-
   void dump_config() override;
+  void setup() override;
+  void update() override;
+
+  // DisplayBuffer methods
+#if ESPHOME_VERSION_CODE >= VERSION_CODE(2022,6,0)
+  display::DisplayType get_display_type() override { return get_greyscale() ? display::DisplayType::DISPLAY_TYPE_GRAYSCALE : display::DisplayType::DISPLAY_TYPE_BINARY; }
+#endif
+  // Draw the actual pixel
+  void draw_absolute_pixel_internal(int x, int y, Color color) override;
 
   void display();
   void clean();
-  void update() override;
 
-  void setup() override;
-
-  uint8_t get_panel_state() { return this->panel_on_; }
-  bool get_greyscale() { return this->greyscale_; }
-
-#if ESPHOME_VERSION_CODE >= VERSION_CODE(2022,6,0)
-  display::DisplayType get_display_type() override {
-    return get_greyscale() ? display::DisplayType::DISPLAY_TYPE_GRAYSCALE : display::DisplayType::DISPLAY_TYPE_BINARY;
-  }
-#endif
+  uint8_t get_panel_state() const { return this->panel_on_; }
+  bool get_greyscale() const { return this->greyscale_; }
 
  protected:
-  void draw_absolute_pixel_internal(int x, int y, Color color) override;
+  int get_width_internal() override { return 960; }
+  int get_height_internal() override { return 540; }
+  size_t get_buffer_length_();
 
   void eink_off_();
   void eink_on_();
 
-
-  int get_width_internal() override { return 960; }
-
-  int get_height_internal() override { return 540; }
-
-  size_t get_buffer_length_();
-
-
   uint8_t panel_on_ = 0;
-  uint8_t temperature_;
-
-  bool greyscale_;
-
+  uint8_t temperature_ = 0;
+  bool greyscale_ = true;
 };
 
-}  // namespace T547
+}  // namespace t547
 }  // namespace esphome
-
-#endif  // USE_ESP32_FRAMEWORK_ARDUINO
